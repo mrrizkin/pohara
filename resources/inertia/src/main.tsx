1890704abfa4359c201f5a069f3122a2837297da
nugrhrizki <nugraha07rizki@gmail.com>
2025-01-19 17:18:59 +0700

    update: loading asset animation

--- resources/inertia/src/main.tsx
+++ resources/inertia/src/main.tsx
@@ -1,15 +1,22 @@
 import { createInertiaApp } from "@inertiajs/react";
+import { Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 
 import "@/assets/css/index.css";
+
+import { Loading } from "@/components/loading";
 
 createInertiaApp({
 	resolve: function (name) {
 		const pages = import.meta.glob("./pages/**/*.tsx");
 		const page = pages[`./pages/${name}.tsx`];
-		return page ? page() : pages["./pages/error/not-found.tsx"]();
+		return page ? lazy(() => page() as Promise<{ default: () => JSX.Element }>) : pages["./pages/error/not-found.tsx"]();
 	},
 	setup({ el, App, props }) {
-		createRoot(el).render(<App {...props} />);
+		createRoot(el).render(
+			<Suspense fallback={<Loading />}>
+				<App {...props} />
+			</Suspense>,
+		);
 	},
 });
