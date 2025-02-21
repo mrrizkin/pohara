ce84acef8cea3bf92392262f899f31e7bf86b97f
nugrhrizki <nugraha07rizki@gmail.com>
2025-02-21 13:55:36 +0700

    update: using inter font family

--- tailwind.admin.config.js
+++ tailwind.admin.config.js
@@ -1,9 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
 	content: ["./resources/admin/**/*.{js,ts,jsx,tsx,html}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+			},
 			borderRadius: {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
